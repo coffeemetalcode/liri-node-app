1613af6040164d185d00497fc7c90d94b1910db5
dbstocker <dstocker@notesettersinc.com>
2018-10-14 13:19:23 -0400

    bands in town is working, spotify is not

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # liri-node-app
+
+A command line app for finding answers to your questions.
+
+Search for:
+* Upcoming concerts
+* Songs from Spotify
+* Movies by title
+* A random concert, Spotify, or movie title search based on text contained in a local file.
+
+*Usage*
+node liri [arg 1, arg 2]
+
+where 'arg 1' is one of the following:
+
+* concert-this
+* spotify-this-song
+* movie-this
+* do-what-it-says
--- liri.js
+++ liri.js
@@ -1,15 +1,31 @@
 // require file with stored environment variables
 require("dotenv").config();
+var Spotify = require("node-spotify-api");
 
-// import the 'keys.js' file
-// var keys = require('keys.js');
+// import the 'keys.js' file for Spotify keys
+var keys = require("./keys");
+
+// request
+var request = require("request");
+
+// fs
+var fs = require("fs");
+
+// moment
+var moment = require('moment');
 
 // grab input
 var mode = process.argv[2];
 var input = process.argv.slice(3).join(" ");
 
-console.log(mode);
-console.log(input);
+// Spotify
+var spotify = new Spotify(keys.spotify);
+
+// divider to separate entries in log.txt
+var divider =
+  "\n------------------------------------------------------------\n\n";
+// console.log(mode);
+// console.log(input);
 
 liri();
 
@@ -17,7 +33,7 @@ function liri() {
   if (mode === "concert-this") {
     concert();
   } else if (mode === "spotify-this-song") {
-    spotify();
+    spotSong();
   } else if (mode === "movie-this") {
     movie();
   } else {
@@ -26,17 +42,53 @@ function liri() {
 }
 
 function concert() {
+  // verify the concert function has been triggered
   console.log("concert function");
-};
 
-function spotify() {
+  var URL = `https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`;
+
+  request(URL, function(error, response, body) {
+    console.log("error:", error);
+    console.log("statusCode:", response && response.statusCode);
+
+    // parse JSON data
+    var jsonData = JSON.parse(body)[0];
+
+    // var concertData = [
+    //   "Artist: " + input,
+    //   "Venue: " + jsonData.venue.name,
+    //   "Location: " + jsonData.venue.city
+    // ];
+
+    var concertData = `Artist: ${input}
+Venue: ${jsonData.venue.name}
+Location: ${jsonData.venue.city}
+Date: ${moment(jsonData.datetime).format('MM/DD/YYYY')}
+    `
+    console.log(concertData);
+
+    fs.appendFile("log.txt", concertData + divider, function(err) {
+      if (err) throw err;
+      console.log(concertData);
+    });
+  });
+}
+
+function spotSong() {
   console.log("spotify function");
-};
+  spotify.search({ type: "track", query: input }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
+}
 
 function movie() {
   console.log("movie function");
-};
+}
 
 function what() {
   console.log("what function");
-};
+}
